fb76b14b233e62e1559de2714d8763b25ec4b488
Hayden Setlik <haydensetlik@gmail.com>
2021-04-30 15:27:03 -0600

    finished frame class

--- Source/Wavetable.h
+++ Source/Wavetable.h
@@ -10,11 +10,16 @@
 
 #pragma once
 #include <JuceHeader.h>
+#include "FFT.h"
 #define TABLESIZE 2048
 #define FFT_ORDER 11
 #define TABLES_PER_FRAME 10
 struct Wavetable
 {
+    Wavetable(int size)
+    {
+        table = new float [size];
+    }
     float* table;
     float minFreq;
     float maxFreq;
@@ -30,7 +35,29 @@ public:
     {
         sampleRate = rate;
     }
+    void createTables(int size, float* real, float* imag);
+    float makeTable(float* waveReal, float* waveImag, int numSamples, float scale, float bottomFreq, float topFreq);
+    Wavetable* tableForHz(double hz);
 private:
     juce::OwnedArray<Wavetable> tables;
+    float phaseDelta;
     double sampleRate;
+    int tablesAdded;
+    int bottomIndex;
+    float bSample;
+    float tSample;
+    float skew;
+};
+
+class WavetableOsc
+{
+public:
+    WavetableOsc(juce::File src);
+    float getSample(double hz, float position);
+    int numFrames;
+private:
+    juce::OwnedArray<WavetableFrame> frames;
+    float position;
+    float phase;
+    float phaseDelta;
 };

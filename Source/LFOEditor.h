062896447be13ca36b7985caabf3a70b11ba909b
Hayden Setlik <haydensetlik@gmail.com>
2021-05-09 16:24:35 -0600

    class for spline handle pairs

--- Source/LFOEditor.h
+++ Source/LFOEditor.h
@@ -158,6 +158,13 @@ protected:
     fPoint relativeCenter;
 };
 
+
+enum class PointType
+{
+    Linear,
+    Bezier,
+    Split
+};
 class CenterHandle : public PointHandle
 {
 public:
@@ -197,11 +204,42 @@ public:
     }
 };
 
-enum class PointType
+class CurveHandlePair : public juce::ComponentListener
 {
-    Linear,
-    Bezier,
-    Split
+public:
+    CurveHandlePair(CenterHandle* center, juce::Component* parent, bool split=false) :
+    linkedCenter(center),
+    parentComp(parent),
+    front(parent),
+    rear(parent),
+    isSplit(split)
+    {
+        front.addComponentListener(this);
+        rear.addComponentListener(this);
+    }
+    void makeVisible()
+    {
+        parentComp->addAndMakeVisible(&front);
+        parentComp->addAndMakeVisible(&rear);
+    }
+    void componentMovedOrResized(juce::Component& comp, bool wasMoved, bool wasResized) override
+    {
+        //! each  move the unclicked handle to match the clicked one relative to the center (if not split ofc)
+        if(&comp == &front && !split)
+        {
+            return;
+        }
+        else if(&comp == &rear && !split)
+        {
+            return;
+        }
+    }
+    CenterHandle* const linkedCenter;
+    juce::Component* const parentComp;
+    CurveHandle front;
+    CurveHandle rear;
+private:
+    bool isSplit;
 };
 
 
@@ -214,13 +252,14 @@ public:
     void paint(juce::Graphics& g) override;
     void resized() override;
     void connectPoints(LFOPoint* first, LFOPoint* second); //!  sets the array's values for this sector of the shape, ensures the corrects paths are drawn
-    void addPoint();
+    void addPoint(); //! TODO: write something to find the largest gap between points and put a point in the middle
     void calculatePaths();
     void removePoint(int idx);
     juce::Rectangle<float> fBounds;
     LFOPoint startPoint;
     LFOPoint endPoint;
     juce::OwnedArray<CenterHandle> pointHandles;
+    juce::OwnedArray<CurveHandlePair> handlePairs;
     juce::OwnedArray<juce::Path> paths;
     lfoArray* const linkedArray;
     lfoArray dataArray;
